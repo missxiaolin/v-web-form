174b986b4cf4880af6f8d92e5945d62ea49b2d66
xiaolin <462441355@qq.com>
2021-06-15 20:01:22 +0800

    * 修改main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue';
+
+
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+app.use(Antd);
+app.use(store);
+
+app.mount('#app');
