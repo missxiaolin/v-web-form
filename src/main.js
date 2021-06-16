58e00f5714ccddea50b939ca38487802f41f3054
xiaolin <462441355@qq.com>
2021-06-16 13:52:22 +0800

    * 组价组件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Antd from 'ant-design-vue';
 
+import {
+    FileImageOutlined,
+    UploadOutlined,
+    PlusOutlined,
+    BarsOutlined,
+    DeleteOutlined,
+    DragOutlined,
+  } from '@ant-design/icons-vue';
+
 
 import router from './router'
 import store from './store'
@@ -11,5 +20,12 @@ const app = createApp(App);
 app.use(router);
 app.use(Antd);
 app.use(store);
+
+app.component('FileImageOutlined', FileImageOutlined);
+app.component('UploadOutlined', UploadOutlined);
+app.component('PlusOutlined', PlusOutlined);
+app.component('BarsOutlined', BarsOutlined);
+app.component('DeleteOutlined', DeleteOutlined);
+app.component('DragOutlined', DragOutlined);
 
 app.mount('#app');
