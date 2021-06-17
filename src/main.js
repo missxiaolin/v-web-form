530bc13c8ff4422a6c2211daa40fb0c6d52ec6f4
xiaolin <462441355@qq.com>
2021-06-17 14:27:07 +0800

    * 初始化项目

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Antd from 'ant-design-vue';
 
+import router from './router'
+import store from './store'
+
 import {
     FileImageOutlined,
     UploadOutlined,
@@ -10,10 +13,6 @@ import {
     DeleteOutlined,
     DragOutlined,
   } from '@ant-design/icons-vue';
-
-
-import router from './router'
-import store from './store'
 
 const app = createApp(App);
 
