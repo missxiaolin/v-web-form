21682da819ee0210df40eed8820622d8673b55bc
xiaolin <462441355@qq.com>
2021-06-19 13:08:28 +0800

    * header组件编写

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <a-spin :spinning="creating" tip="Loading...">
+    <Header />
   </a-spin>
 </template>
 
 <script>
+import Header from '@/components/header';
 import { reactive, toRefs } from 'vue'
 
 export default {
   name: 'Home',
   components: {
+    Header
   },
   setup() {
     const state = reactive({
