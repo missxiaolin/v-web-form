8d11e9f4abcfdcd6719d69ab7274f1ce2f8947e5
xiaolin <462441355@qq.com>
2021-06-20 15:11:04 +0800

    * 引入ant

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
 import router from './router'
 import store from './store'
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,60 @@
 <template>
   <a-spin :spinning="creating" tip="Loading...">
     <Header />
+    <div class="index-wrapper">
+      <a-card
+        :key="item.id"
+        v-for="item in templateList"
+        hoverable
+        style="width: 240px"
+        @click="() => initPage(item)"
+      />
+      <template>
+        <img
+          alt="example"
+          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+        />
+      </template>
+    </div>
   </a-spin>
 </template>
 
 <script>
-import Header from '@/components/header';
-import { reactive, toRefs } from 'vue'
+import Header from "@/components/header";
+import { reactive, toRefs } from "vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    Header
+    Header,
   },
   setup() {
     const state = reactive({
+      templateList: [],
+      createDialog: false,
+      pageInfo: {},
       creating: false,
-    })
+    });
+
+    const initPage = (config) => {
+      state.pageInfo = config;
+      state.createDialog = true;
+    };
 
     return {
-      ...toRefs(state)
-    }
-  }
-}
+      ...toRefs(state),
+      initPage,
+    };
+  },
+};
 </script>
+
+<style lang="scss">
+.index-wrapper {
+  width: 100%;
+  height: 100vh;
+  background: #f5f5f5;
+  padding: 20px;
+  box-sizing: border-box;
+}
+</style>
