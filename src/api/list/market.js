c5c8fbb71ac476bd3892c6083ffbd7eb6168a79f
xiaolin <462441355@qq.com>
2021-06-30 16:00:20 +0800

    * add

--- src/api/list/market.js
+++ src/api/list/market.js
@@ -2,11 +2,11 @@ import instance from '../request';
 
 export default {
     async queryAll() {
-        const result = await instance.get('/template/list');
+        const result = await instance.get('/formwork/list');
         return result.data;
     },
     async queryInfo(params) {
-        const result = await instance.get('/template/detail', { params });
+        const result = await instance.get('/formwork/query', { params });
         return result.data;
     }
 }
