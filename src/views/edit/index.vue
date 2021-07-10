b50dc8ab2691321116c669d5ccbe58e839d3e935
xiaolin <462441355@qq.com>
2021-07-10 21:45:58 +0800

    * 初始化vue

--- src/views/edit/index.vue
+++ src/views/edit/index.vue
@@ -1,15 +1,29 @@
 <template>
-  <Header type="white" class="edit-menu"> 凄凄切切 </Header>
+  <Header type="white" class="edit-menu">
+    <template #pageTitle>
+      <div class="page-title">
+        
+      </div>
+    </template>
+  </Header>
   <div>edit</div>
 </template>
 
 
 <script>
 import Header from "@/components/header";
+import { SettingOutlined } from "@ant-design/icons-vue";
+import { useStore } from "vuex";
+import { toRefs } from "vue";
+
 export default {
   name: "Edit",
+  setup() {
+    
+  },
   components: {
     Header,
+    SettingOutlined,
   },
 };
 </script>
