2c9532088d2ca66fed566e06a2659f6c7e23495a
xiaolin <462441355@qq.com>
2021-07-25 21:07:29 +0800

    * 更改目录

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Index from '../views/index.vue'
+import Index from '../views/index/index.vue'
 import Dashboard from '../views/dashboard/index.vue';
 import Edit from '../views/edit/index';
 
