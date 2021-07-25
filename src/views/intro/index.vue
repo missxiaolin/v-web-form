9ffbd27d9a711ed85040027cbf831e26002039c3
xiaolin <462441355@qq.com>
2021-07-25 21:40:22 +0800

    * add

---
+++ src/views/intro/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="pg-index">
+      <Header/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/header';
+
+export default {
+  name: "intro",
+  components: {
+      Header
+  }
+};
+</script>
+
+<style lang="scss">
+#pg-index {
+  width: 100%;
+  background: #fff;
+  overflow-x: hidden;
+
+}
+</style>
