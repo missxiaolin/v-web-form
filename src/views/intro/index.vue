2c4cfcc1074cbe3a179c43fb59ac528550f139df
xiaolin <462441355@qq.com>
2021-07-27 17:44:24 +0800

    * add

--- src/views/intro/index.vue
+++ src/views/intro/index.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="pg-index">
-      <Header/>
+    <Header />
+    <div class="kaer-home-banner">
+      <div class="fd-home-banner-content">
+        <div class="fd-home-banner-content-info">
+          <h1 class="" style="transform: translate(0px, 0px); opacity: 1">
+            极简制作，一键呈现
+          </h1>
+          <p class="" style="transform: translate(0px, 0px); opacity: 1">
+            海量组件、模板，随意使用<br />
+            支持自定义开发组件、模板，满足不同场景的业务诉求
+          </p>
+          <div class="" style="transform: translate(0px, 0px); opacity: 1">
+            <router-link to="/dashboard" data-external="true">
+              <button type="button" class="fdAnt-btn fd-guide-btn">
+                <span>开始使用</span>
+              </button>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Header from '@/components/header';
+import Header from "@/components/header";
 
 export default {
   name: "intro",
   components: {
-      Header
-  }
+    Header,
+  },
 };
 </script>
 
@@ -20,6 +40,5 @@ export default {
   width: 100%;
   background: #fff;
   overflow-x: hidden;
-    
 }
 </style>
